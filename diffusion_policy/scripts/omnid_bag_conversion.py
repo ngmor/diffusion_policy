b59743edb61320e214e1e19047567790d6937cb7
Nick Morales <ngmorales97@gmail.com>
2023-10-31 00:19:42 -0500

    Successfully convert decimated ROS bags to zarr, but only with joint states for now

--- diffusion_policy/scripts/omnid_bag_conversion.py
+++ diffusion_policy/scripts/omnid_bag_conversion.py
@@ -1,10 +1,14 @@
 from typing import Any
-
 from enum import Enum
 from enum import auto as enum_auto
+import pathlib
+import numpy as np
+
 import hydra
 from omegaconf import OmegaConf
-import pathlib
+
+import sensor_msgs.msg
+
 from diffusion_policy.common.replay_buffer import ReplayBuffer
 
 from omnid_bag import decimate
@@ -15,16 +19,23 @@ OmegaConf.register_new_resolver("eval", eval, replace=True)
 PACKAGE_PATH = pathlib.Path(__file__).parent.parent.parent
 
 class DataType(Enum):
+    TIMESTEP = enum_auto()
     ACTION = enum_auto()
     LOW_DIM_OBS = enum_auto()
     IMG = enum_auto()
 
 class DataLocator:
     def __init__(self, dtype: DataType, ndx: int):
-        self.dtype = dtype
-        self.ndx = ndx
+        self._dtype = dtype
+        self._ndx = ndx
     def __repr__(self) -> str:
         return f'({self.dtype.name}, {self.ndx})'
+    @property
+    def dtype(self) -> DataType:
+        return self._dtype
+    @property
+    def ndx(self) -> int:
+        return self._ndx
 
 def add_scalar_attribute(obj: Any, attr: str, namespace: str, locator_dict: dict, low_dim_obs_list: list, actions_list: list):
     if hasattr(obj, attr):
@@ -53,15 +64,15 @@ def main(cfg: OmegaConf):
     output_dir = pathlib.Path(cfg.output_path)
     assert output_dir.parent.is_dir()
     zarr_path = str(output_dir.joinpath(output_dir.name + '.zarr').absolute())
-    replay_buffer = ReplayBuffer.create_from_path(zarr_path=zarr_path, mode='a')
+    replay_buffer = ReplayBuffer.create_from_path(zarr_path=zarr_path, mode='w')
 
     # Determine what data will go where in the inputs to the model
     topics = []
-    low_dim_obs = []
-    actions = []
+    low_dim_obs_names = []
+    actions_names = []
 
+    # Determine where to place joint state data according to configuration
     joint_states = {}
-
     if hasattr(cfg, 'joint_states'):
         for topic in cfg.joint_states:
             topics.append(topic.topic)
@@ -78,24 +89,24 @@ def main(cfg: OmegaConf):
                     attr='position',
                     namespace=namespace,
                     locator_dict=joint_states[topic.topic][joint.name],
-                    low_dim_obs_list=low_dim_obs,
-                    actions_list=actions
+                    low_dim_obs_list=low_dim_obs_names,
+                    actions_list=actions_names
                 )
                 add_scalar_attribute(
                     obj=joint,
                     attr='velocity',
                     namespace=namespace,
                     locator_dict=joint_states[topic.topic][joint.name],
-                    low_dim_obs_list=low_dim_obs,
-                    actions_list=actions
+                    low_dim_obs_list=low_dim_obs_names,
+                    actions_list=actions_names
                 )
                 add_scalar_attribute(
                     obj=joint,
                     attr='effort',
                     namespace=namespace,
                     locator_dict=joint_states[topic.topic][joint.name],
-                    low_dim_obs_list=low_dim_obs,
-                    actions_list=actions
+                    low_dim_obs_list=low_dim_obs_names,
+                    actions_list=actions_names
                 )
 
     # Collect data from bags (each is an episode)
@@ -103,18 +114,67 @@ def main(cfg: OmegaConf):
         # Skip anything that isn't a directory since this isn't a ROS bag
         if not bag.is_dir():
             continue
-        
+
         # Decimate data according to configured rate
         episode_data = decimate(str(bag), topics, cfg.rate)
 
+        # Map from data types to output data arrays
+        data_arrays = {
+            DataType.TIMESTEP: np.zeros((len(episode_data), 1), dtype=np.float32),
+            DataType.LOW_DIM_OBS: np.zeros((len(episode_data), len(low_dim_obs_names)), dtype=np.float32),
+            DataType.ACTION: np.zeros((len(episode_data), len(actions_names)), dtype=np.float32),
+        }
 
-        # TODO remove
-        break
+        # interpret all data for each timestep
+        for t in range(len(episode_data)):
+            data_frame = episode_data[t]
 
-    # print(topics)
-    # print(joint_states)
-    # print(low_dim_obs)
-    # print(actions)
+            topics_ndx_offset = 0
+
+            # Process joint states
+            for i in range(len(joint_states.keys())):
+                # Index in topics and data frame
+                ndx = topics_ndx_offset + i
+
+                # Map from joint names to where the data should be placed
+                joint_state_map = joint_states[topics[ndx]]
+
+                # Get message from data frame
+                joint_state_msg: sensor_msgs.msg.JointState = data_frame[ndx]
+                
+                for j, joint in enumerate(joint_state_msg.name):
+                    if not joint in joint_state_map.keys():
+                        continue
+
+                    # For each attribute, a locator should be in the joint_state_map[joint]
+                    # dictionary.
+                    # locator.dtype gives the data type (action, low_dim_obs, etc) which
+                    # determines which data_array the data will go into
+                    # locator.ndx gives the index in that data_array this value should go
+                    if 'position' in joint_state_map[joint].keys():
+                        locator = joint_state_map[joint]['position']
+                        data_arrays[locator.dtype][t, locator.ndx] = joint_state_msg.position[j]
+                    if 'velocity' in joint_state_map[joint].keys():
+                        locator = joint_state_map[joint]['velocity']
+                        data_arrays[locator.dtype][t, locator.ndx] = joint_state_msg.velocity[j]
+                    if 'effort' in joint_state_map[joint].keys():
+                        locator = joint_state_map[joint]['effort']
+                        data_arrays[locator.dtype][t, locator.ndx] = joint_state_msg.effort[j]
+
+            # Process timesteps
+            data_arrays[DataType.TIMESTEP][t] = data_frame[-1]
+
+        # Format episode data for inclusion in replay buffer
+        episode_data_dict = {
+            'timestep': data_arrays[DataType.TIMESTEP],
+            'low_dim_obs': data_arrays[DataType.LOW_DIM_OBS],
+            'action': data_arrays[DataType.ACTION],
+        }
+
+        # Add episode to replay buffer
+        replay_buffer.add_episode(episode_data_dict, compressors='disk')
+
+    print(f'Converted {replay_buffer.n_episodes} episodes.')
 
 if __name__ == "__main__":
     main()
