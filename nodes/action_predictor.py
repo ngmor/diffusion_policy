9cc5d4e17602d01c9a497642049ede707e0a8d33
Nick Morales <ngmorales97@gmail.com>
2023-11-03 13:24:14 -0500

    Some cleanup of action predictor node

--- nodes/action_predictor.py
+++ nodes/action_predictor.py
@@ -11,6 +11,7 @@ from diffusion_policy.common.ros_util import ROSDataConverter
 
 import rclpy
 from rclpy.node import Node
+from rclpy.parameter import Parameter
 import message_filters
 
 class LastMessageSubscriber:
@@ -42,6 +43,9 @@ class ActionPredictor(Node):
         # self.declare_parameter('checkpoint_path', 'src/diffusion_policy/data/outputs/2023.11.03/01.22.46_train_diffusion_unet_lowdim_omnid_lowdim/checkpoints/epoch=0000-test_mean_score=0.000.ckpt')
         checkpoint_path = self.get_parameter('checkpoint_path').get_parameter_value().string_value
 
+        self.declare_parameter('num_inference_diffusion_timesteps', 16)
+        self.num_inference_diffusion_timesteps = self.get_parameter('num_inference_diffusion_timesteps').get_parameter_value().integer_value
+
         # TODO method of disabling specific cameras
 
         # # Load payload/workspace
@@ -51,33 +55,33 @@ class ActionPredictor(Node):
         self.observation_rate = self.cfg.task.data_conversion.rate
         self.observation_period = 1.0 / self.observation_rate
 
-        # TODO uncomment
-        # workspace_cls = hydra.utils.get_class(self.cfg._target_)
-        # self.workspace: BaseWorkspace = workspace_cls(self.cfg)
-        # self.workspace.load_payload(self.payload, exclude_keys=None, include_keys=None)
+        workspace_cls = hydra.utils.get_class(self.cfg._target_)
+        self.workspace: BaseWorkspace = workspace_cls(self.cfg)
+        self.workspace.load_payload(self.payload, exclude_keys=None, include_keys=None)
 
-        # # Load model
-        # # Type hint is fine even if the policy is only low dim
-        # self.policy: BaseImagePolicy = self.workspace.model
-        # if self.cfg.training.use_ema:
-        #     self.policy = self.workspace.ema_model
+        # Load model
+        # Type hint is fine even if the policy is only low dim
+        self.policy: BaseImagePolicy = self.workspace.model
+        if self.cfg.training.use_ema:
+            self.policy = self.workspace.ema_model
 
-        # self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # GPU
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # GPU
 
-        # self.policy.eval().to(self.device)
+        self.policy.eval().to(self.device)
 
-        # # Set interface parameters
-        # if 'DDIM' in self.cfg.policy.noise_scheduler._target_:
-        #     self.policy.num_inference_steps = self.num_inference_steps
-        # else:
-        #     self.get_logger().info('DDIM noise scheduler not used, ignoring inference steps parameter')
-        #     self.num_inference_steps = self.policy.num_inference_steps
-        #     self.set_parameters([Parameter(name='num_inference_steps', value=self.num_inference_steps)])
+        # Set interface parameters
+        if 'DDIM' in self.cfg.policy.noise_scheduler._target_:
+            self.policy.num_inference_steps = self.num_inference_diffusion_timesteps
+        else:
+            self.get_logger().info('DDIM noise scheduler not used, ignoring inference steps parameter')
+            self.num_inference_diffusion_timesteps = self.policy.num_inference_steps
+            self.set_parameters([Parameter(name='num_inference_diffusion_timesteps', value=self.num_inference_diffusion_timesteps)])
 
-        # self.policy.n_action_steps = self.policy.horizon - self.policy.n_obs_steps + 1
+        self.policy.n_action_steps = self.policy.horizon - self.policy.n_obs_steps + 1
 
-        # self.get_logger().info('Successfully loaded model.')
+        self.get_logger().info('Successfully loaded model.')
 
+        # Init data converter
         self.data_converter = ROSDataConverter(
             cfg=self.cfg.task.data_conversion,
             disable_cameras=self.low_dim
@@ -97,29 +101,14 @@ class ActionPredictor(Node):
         self.received = np.full((len(self.last_message),), False)
         self.timer_input_data = self.create_timer(self.observation_period, self.timer_observation_callback)
         self.at_least_one_received = False
+        self.n_obs_received = False
 
-        # input_data_subscribers = []
-
-        # for topic in topics:
-        #     input_data_subscribers.append(message_filters.Subscriber(
-        #         self,
-        #         topic_info[topic]['type'],
-        #         topic
-        #     ))
-        # self.ats = message_filters.ApproximateTimeSynchronizer(
-        #     input_data_subscribers,
-        #     self.cfg.n_obs_steps,
-        #     self.observation_period,
-        #     allow_headerless=True
-        # )
-        # self.ats.registerCallback(self.synchronized_observation_callback)
 
         self.last_obs_callback_time = self.get_clock().now()
         self.input_data_queue = []
         self.get_logger().info(f'Synchronizing at {self.observation_rate} Hz')
 
     def timer_observation_callback(self):
-        receive_time = self.get_clock().now()
         if np.any(~self.received):
             if not self.at_least_one_received:
                 return
@@ -132,24 +121,10 @@ class ActionPredictor(Node):
         if len(self.input_data_queue) >= self.cfg.n_obs_steps:
             self.input_data_queue = self.input_data_queue[1:] + [self.last_message]
         else:
+            if not self.n_obs_received:
+                self.get_logger().info(f'{self.cfg.n_obs_steps} observations received, ready to begin inference.')
+                self.n_obs_received = True
             self.input_data_queue.append(self.last_message)
-
-        print(1.0e9 / (receive_time - self.last_obs_callback_time).nanoseconds, len(self.input_data_queue))
-        self.last_obs_callback_time = receive_time
-
-    def synchronized_observation_callback(self, *msgs):
-        receive_time = self.get_clock().now()
-        if (receive_time - self.last_obs_callback_time).nanoseconds / 1.0e9 \
-        < self.observation_period:
-            return
-
-        if len(self.input_data_queue) >= self.cfg.n_obs_steps:
-            self.input_data_queue = self.input_data_queue[1:] + [msgs]
-        else:
-            self.input_data_queue.append(msgs)
-
-        print(1.0e9 / (receive_time - self.last_obs_callback_time).nanoseconds, len(self.input_data_queue))
-        self.last_obs_callback_time = receive_time
 
 def main(args=None):
     rclpy.init(args=args)
